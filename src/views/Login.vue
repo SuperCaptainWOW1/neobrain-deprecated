a30f13eb838d764def7f14e4de81bb69c7ba5bb9
Vladimir Makhortov <makhortov.dev@gmail.com>
2020-02-29 14:13:08 +0300

    Add Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="email" type="text" name="email" />
+      <br />
+      <input v-model="password" type="password" name="password" />
+      <br />
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<script>
+import router from 'vue-router';
+
+export default {
+  name: 'Login',
+  data: () => ({
+    email: 'user@email.com',
+    password: 'password',
+  }),
+  methods: {
+    login() {
+      const loginData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      try {
+        fetch('/api/login', {
+          method: 'POST',
+          body: loginData,
+        });
+        console.log('Logged in');
+        router.push('/chat');
+      } catch (err) {
+        console.log('Cannot log in');
+      }
+    },
+  },
+};
+</script>
